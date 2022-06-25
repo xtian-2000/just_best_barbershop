7e038555afa75d84ea74508177ab2673a989be3f
xtian-2000 <christian.gealone1@gmail.com>
2022-06-25 14:53:21 +0800

    created script for popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 function goto_book_now() {
     location.replace("book_now.html")
 }
+
+function appointment_success() {
+    alert("Your appointment is already being processed, please wait for an email or call within business hours");
+}
